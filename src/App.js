f8bb9002ac30f3d333ca0be66f77ae8317378b1c
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-21 12:28:08 +0800

    Revert App to function component

--- src/App.js
+++ src/App.js
@@ -2,29 +2,27 @@ import React from 'react';
 import './App.css';
 import Counter from "./components/Counter";
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
 
-                  <Counter/>
+                <Counter/>
 
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn Reacc
-                    </a>
-                </header>
-            </div>
-        );
-    }
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn Reacc
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
